6ff537386e1ff0ce4a18eed20c6cda1fa830d0d9
ardas <adrian17159@gmail.com>
2022-02-21 13:59:42 +0100

    base phase

---
+++ src/main/java/pl/ardas/springsecuritypractising/config/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package pl.ardas.springsecuritypractising.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
