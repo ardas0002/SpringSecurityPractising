1ce545b5a16d2752f601d5da02ec1170d52d23a5
ardas <adrian17159@gmail.com>
2022-02-21 12:49:12 +0100

    init

---
+++ src/main/java/pl/ardas/springsecuritypractising/SpringSecurityPractisingApplication.java
@@ -0,0 +1,13 @@
+package pl.ardas.springsecuritypractising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractisingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractisingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ardas/springsecuritypractising/SpringSecurityPractisingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ardas.springsecuritypractising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPractisingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
